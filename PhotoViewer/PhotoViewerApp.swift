bc30b5d0e49a6f1caba16ad5903b5124fac5ac60
Arina <karma.rin512@gmail.com>
2023-06-15 16:57:37 +0300

    Initial Commit

---
+++ PhotoViewer/PhotoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoViewerApp.swift
+//  PhotoViewer
+//
+//  Created by Арина Моргачева on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
